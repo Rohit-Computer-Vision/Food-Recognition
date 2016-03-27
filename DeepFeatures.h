6257f960a40bbe7c3277a6a2afbce808db75e58d
Rohit Nair <rohitnair987@gmail.com>
2016-03-27 04:40:24 -0400

    Converted output as required by svm
    output model is deep_model_svm.txt

--- DeepFeatures.h
+++ DeepFeatures.h
@@ -11,6 +11,16 @@ public:
   // them to a common size, convert to greyscale, and dump them as vectors to a file
   virtual void train(const Dataset &filenames) 
   {
+	//to delete older model files
+	string del = "rm -f deep_model_svm.txt";
+	system(del.c_str());
+
+	//storing all folder names for category id used for svm format conversion later
+	string folders[filenames.size()];
+	int count = 0;
+	for(Dataset::const_iterator c_iter=filenames.begin(); c_iter != filenames.end(); ++c_iter)
+		folders[count++] = c_iter->first;
+
     for(Dataset::const_iterator c_iter=filenames.begin(); c_iter != filenames.end(); ++c_iter)
       {
 	cout << "Processing " << c_iter->first << endl;
@@ -20,27 +30,68 @@ public:
 	for(int i=0; i<c_iter->second.size(); i++)
 	  class_vectors = class_vectors.draw_image(0, i, 0, 0, extract_features(c_iter->second[i].c_str()));
 	
-	string filename;
-	string command;
-	for(int i=0; i<c_iter->second.size(); i++){
-		filename = "deep_model_" + c_iter->first + "_" + c_iter->second[0].substr(7+c_iter->first.length(),c_iter->second[0].length()-4)+".txt";
-		cout << filename << endl;
-		freopen(filename.c_str(),"w",stdout);
-		cout << "file opened" << endl;
-		command = "./overfeat/bin/linux_64/overfeat -L 12 " + c_iter->second[i];
-		cout << command << endl;
-		system(command.c_str());
-		fclose(stdout);
-	}
-	cout << endl;
+	//creating models
+	string filename, command;
+	int start, len;
+//	for(int i=0; i<c_iter->second.size(); i++){
+//		start = 7+c_iter->first.length();
+//		len = c_iter->second[i].length()-start-4;
+//		filename = "deep_model_" + c_iter->first + "_" + c_iter->second[i].substr(start,len)+".txt";
+//		cout << filename << endl;
+//		freopen(filename.c_str(),"w",stdout);
+//		command = "./overfeat/bin/linux_64/overfeat -L 12 " + c_iter->second[i];
+//		system(command.c_str());
+//		fclose(stdout);
+//		freopen("/dev/tty", "a", stdout);
+//	}
+//	cout << endl;
 
+	//modifying models for svm
+	ifstream myReadFile;
+	ofstream myWriteFile;
+	int no_of_features, feature_height, feature_width, next, feature_no = 1, image_no = 0;
 
-//	cout << c_iter->first << endl;
-//	for(int i=0; i<c_iter->second.size(); i++)
-//		cout << c_iter->second[i].c_str() << endl;
+	for(int file_no = 0; file_no < c_iter->second.size(); file_no++) { //50
+		start = 7 + c_iter->first.length();
+		len = c_iter->second[file_no].length()-start-4;
+		filename = "deep_model_" + c_iter->first + "_" + c_iter->second[file_no].substr(start,len)+"_svm.txt";
+		myReadFile.open(("deep_model_" + c_iter->first + "_" + c_iter->second[file_no].substr(start,len) + ".txt").c_str());
+		myWriteFile.open("deep_model_svm.txt", ofstream::out | ofstream::app);
+		char output[20];
+		if (myReadFile.is_open()) {
+			myReadFile >> no_of_features;
+			myReadFile >> feature_height;
+			myReadFile >> feature_width;
+			next = feature_height * feature_width;
 
-//	class_vectors.save_png(("deep_model_" + c_iter->first + ".png").c_str());
+			//calculate category number of food product
+			for(int count=0;count<filenames.size();count++)
+				if(folders[count].compare(c_iter->first) == 0) {
+					image_no = count;
+					break;
+				}
+
+			myWriteFile << image_no << ' ';
+			image_no++;
+			int i = 0;
+			feature_no = 1;
+			while (!myReadFile.eof()) {
+				myReadFile >> output;
+				myWriteFile << feature_no << ':';
+				feature_no++;
+				myWriteFile << output << ' ';
+				for(int skip = 0; skip < next; skip++)
+					myReadFile >> output;
+			}
+			myWriteFile << ' ' << '#' << ' ' << c_iter->second[file_no] << '\n';
+		}
+		myReadFile.close();
+		myWriteFile.close();
       }
+      }
+    //deleting unnecessary model files
+//    string del = "rm -f deep_model_*_.txt";
+//	system(del.c_str());
   }
 
   virtual string classify(const string &filename)
@@ -61,7 +112,7 @@ public:
   virtual void load_model()
   {
     for(int c=0; c < class_list.size(); c++)
-      models[class_list[c] ] = (CImg<double>(("Deep_model_" + class_list[c] + ".png").c_str()));
+      models[class_list[c] ] = (CImg<double>(("deep_model_" + class_list[c] + ".txt").c_str()));
   }
 protected:
   // extract features from an image, which in this case just involves resampling and 
