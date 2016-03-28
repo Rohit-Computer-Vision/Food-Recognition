7ba538daf8569a666acb95ad21d9a6e2e75bdf49
Rohit Nair <rohitnair987@gmail.com>
2016-03-27 20:59:42 -0400

    updated svm

--- svm.h
+++ svm.h
@@ -6,8 +6,6 @@ public:
   svm(const vector<string> &_class_list) : Classifier(_class_list) {}
   ofstream myfile;
   
-  // Nearest neighbor training. All this does is read in all the images, resize
-  // them to a common size, convert to greyscale, and dump them as vectors to a file
   virtual void train(const Dataset &filenames) 
   {
   	//cout<<filenames<<endl;
@@ -18,7 +16,7 @@ public:
 
         p=p+1;
 		cout << "Processing " << c_iter->first << endl;
-		CImg<double> class_vectors(40*40*3, filenames.size(), 1);
+		CImg<double> class_vectors(30*30*3, filenames.size(), 1);
 	
 	// convert each image to be a row of this "model" image
 	for(int i=0; i<c_iter->second.size(); i++){
@@ -33,13 +31,13 @@ public:
 			myfile << k << ":" << class_vectors[j] << " " ;
 		}
 	
-		myfile <<"\n";
+		myfile <<'\n';
 	}
 	
       }
 	myfile.close();
   
-  system(" ./svm_multiclass_learn -c 1.0 dat.txt");
+  system(" ./svm_multiclass_learn -c 2000 dat.txt");
   }
 
 int dir_count=1;
@@ -65,14 +63,16 @@ virtual string classify(const string &filename)
 	for( int j = 0; j < test_image.size(); j++)
 			myfile << j+1 << ":"<< test_image[j] << " " ;
 	
-	myfile <<"\n";
+	myfile <<'\n';
 	myfile.close();
     system("./svm_multiclass_classify testing.txt svm_struct_model");
 	
-    std::ifstream input("svm_predictions");
+    fstream myfile_input("svm_predictions", std::ios_base::in);
     int x;
-    input>>x;
-    // cout<<x;
+    myfile_input>>x;
+    cout<<x<<endl;
+    cout<<x-1<<endl;
+    myfile_input.close();
     return class_list[x-1];
 	
 }
@@ -87,7 +87,7 @@ protected:
   // rearranging into a vector of pixel data.
   CImg<double> extract_features(const string &filename)
     {
-      return (CImg<double>(filename.c_str())).resize(40,40,1,3).unroll('x');
+      return (CImg<double>(filename.c_str())).resize(30,30,1,3).unroll('x');
     }
 
   //static const int size=20;  // subsampled image resolution
